5e8146bc79e6f67a3d47c0b37a909afaace0d65e
DavidFnck <davidfnck@gmail.com>
2017-08-01 11:45:44 +0800

    修改密码泄漏问题

--- index/gethint.php
+++ index/gethint.php
@@ -34,16 +34,6 @@ try {
 //lookup all hints from array if length of q>0
 if (strlen($q) > 0)
 {
-    // 这种数据库链接方案已经被弃用
-    // $con = mysql_connect("localhost","root","6n2]E,(tUN4BqZDH$W/}$3>nM6rV#ze2GWft3Fg$8TDAj9bw4B");
-    // if(!$con)
-    // {
-    //     die('Could not connect: ' .mysql_error());
-    // }
-    // mysql_select_db("ezrhyme_db",$con);
-
-    // 老版本中读取 MySQL 的时候出现的字符编码问题，需要通过这个方式解决
-    // $result =mysql_query('SET character_set_connection=utf8, character_set_results=utf8, character_set_client=binary',$con);
 
     $sql = "select * from ezrhyme_01";
     //$sql="SELECT FirstName FROM Persons where Firstname like '%$q%'";
